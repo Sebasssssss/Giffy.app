8638e3f195e37a4f9abcb9eda139dae572eb5f86
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-01-11 23:49:02 -0300

    Lorem text

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,20 +3,29 @@ import React from 'react'
 import CardsAbout from '../../components/ListOfGifs'
 import LazyTrending from '../../components/TrendingSearches'
 import { Helmet } from 'react-helmet'
+import { IoArrowForward } from 'react-icons/io5/'
 
 export default function Home() {
   const { gifs } = useGifs()
+
   return (
     <>
       <Helmet>
         <title>Home | Giffy</title>
       </Helmet>
       <p className="mx-auto mt-16 max-w-3xl p-5">
-        Im Sebastian and ive been into the tecnology world since im 13 years
-        old. I started coding due to friends who would usually code and because
-        i used to watch many videos of people coding, as Takuya Matsuyama.
-        Nowadays Im coding php and using Tailwindcss as a tool for making my
-        websites.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti id
+        nihil, at dolorum eaque maiores quia amet sit eius libero nulla ipsam
+        perferendis consectetur obcaecati fugiat, odio quas non facere dicta
+        nisi nobis accusantium! doloribus libero inventore delectus animi quasi
+        perferendis consectetur obcaecati fugiat, odio quas non facere dicta
+        nisi nobis accusantium! doloribus libero inventore delectus animi quasi
+        voluptatibus possimus excepturi,{' '}
+        <span className="inline-flex items-center gap-1 text-[#f6c177] transition-colors duration-500 dark:text-[#31748f]">
+          {' '}
+          voluptates et distinctio. Repellat?
+          <IoArrowForward />
+        </span>
       </p>
       <CardsAbout gifs={gifs} />
       <LazyTrending />
