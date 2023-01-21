70ad42751d5f5dc9a765b9267a462b0008b82aba
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-01-21 18:28:11 -0300

    Compressed main video. Now it's just 2mb

--- src/components/mainVideo/index.jsx
+++ src/components/mainVideo/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import video from '../../assets/mainVideo.mp4'
+import video from '../../assets/videoMain.mp4'
 import SearchForm from '../../components/SearchForm'
 
 export default function Main() {
