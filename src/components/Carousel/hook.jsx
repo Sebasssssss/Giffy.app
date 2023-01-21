c0776caa72abfa89939b0c585585f11fa2f5950e
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-01-21 00:36:37 -0300

    Added spacing between elements in carousel. Changed carousel dots color

--- src/components/Carousel/hook.jsx
+++ src/components/Carousel/hook.jsx
@@ -15,20 +15,25 @@ export default function carouselHook() {
   }
 
   const trendingGifItems = trendingGifs.map(({ title, id, url }) => (
-    <TrendingGifs key={id} title={title} id={id} url={url} />
+    <div key={id} className="mx-2">
+      <TrendingGifs title={title} id={id} url={url} />
+    </div>
   ))
 
   const trendingStickerItems = stickers.map(({ id, url }) => (
-    <Link to="/stickers/trending/" key={id}>
-      <div className="mb-4 flex cursor-pointer items-center overflow-hidden rounded-md border border-blue-900/20 text-center shadow-card transition-all duration-200 hover:shadow-cardHover dark:border-[#3E2C41] dark:shadow-cardDark dark:hover:shadow-cardDarkHover">
+    <div
+      key={id}
+      className="mx-2 mb-4 flex cursor-pointer items-center overflow-hidden rounded-md border border-blue-900/20 text-center shadow-card transition-all duration-200 hover:shadow-cardHover dark:border-[#3E2C41] dark:shadow-cardDark dark:hover:shadow-cardDarkHover"
+    >
+      <Link to="/stickers/trending/">
         <img
           src={url}
           loading="lazy"
           decoding="async"
           className="h-80 w-full object-cover md:h-44"
         />
-      </div>
-    </Link>
+      </Link>
+    </div>
   ))
   return { trendingStickerItems, trendingGifItems, responsive }
 }
